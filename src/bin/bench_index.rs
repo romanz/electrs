946bff4c0e2f79bd4cc650a8fa4e7fae3f4bfb51
Roman Zeyde <me@romanzey.de>
2018-06-09 18:22:30 +0300

    Remove logging from index benchmark

--- src/bin/bench_index.rs
+++ src/bin/bench_index.rs
@@ -1,11 +1,5 @@
 extern crate electrs;
-
 extern crate error_chain;
-
-#[macro_use]
-extern crate log;
-
-use error_chain::ChainedError;
 
 use electrs::{config::Config,
               daemon::Daemon,
@@ -13,6 +7,7 @@ use electrs::{config::Config,
               index::Index,
               store::{ReadStore, Row, WriteStore},
               util::Bytes};
+use error_chain::ChainedError;
 
 struct FakeStore;
 
@@ -32,16 +27,14 @@ impl WriteStore for FakeStore {
 fn run() -> Result<()> {
     let config = Config::from_args();
     let daemon = Daemon::new(config.network_type)?;
-    debug!("{:?}", daemon.getblockchaininfo()?);
     let fake_store = FakeStore {};
     let index = Index::load(&fake_store);
-    let tip = index.update(&fake_store, &daemon)?;
-    info!("downloaded and indexed till {}", tip);
+    index.update(&fake_store, &daemon)?;
     Ok(())
 }
 
 fn main() {
     if let Err(e) = run() {
-        error!("{}", e.display_chain());
+        eprintln!("{}", e.display_chain());
     }
 }
