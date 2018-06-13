a6c9912c55e8f6eb4d7da774ba9e704c83d389de
Roman Zeyde <me@romanzey.de>
2018-06-13 12:40:21 +0300

    Start monitoring server at benchmark tool

--- src/bin/bench_index.rs
+++ src/bin/bench_index.rs
@@ -31,6 +31,8 @@ fn run() -> Result<()> {
     let signal = Waiter::new();
     let config = Config::from_args();
     let metrics = Metrics::new(config.monitoring_addr);
+    metrics.start();
+
     let daemon = Daemon::new(config.network_type, &metrics)?;
     let fake_store = FakeStore {};
     let index = Index::load(&fake_store, &metrics);
