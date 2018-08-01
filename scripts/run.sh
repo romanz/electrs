87b4df57472ad6fdb1b1f09cff0dbd03c29b822d
Roman Zeyde <me@romanzey.de>
2018-08-01 11:07:30 +0300

    Persist stderr log with run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -eu
+trap 'kill $(jobs -p)' EXIT
 
-T=5
+DELAY=5
+LOG=/tmp/electrs.log
+tail -v -n0 -F "$LOG" &
+
 export RUST_BACKTRACE=1
 while :
 do
 	cargo fmt
 	cargo check --release
-	cargo run --release -- $*
-	echo "Restarting in $T seconds..."
-	sleep $T
+	cargo run --release -- $* 2>> "$LOG"
+	echo "Restarting in $DELAY seconds..."
+	sleep $DELAY
 done
