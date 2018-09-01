fcd85e9f516a88902ff0eb81210bf9a4decf6428
Roman Zeyde <me@romanzey.de>
2018-09-01 10:13:46 +0300

    Add a small test for FakeStore

--- src/fake.rs
+++ src/fake.rs
@@ -16,3 +16,21 @@ impl WriteStore for FakeStore {
     fn write(&self, _rows: Vec<Row>) {}
     fn flush(&self) {}
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_fakestore() {
+        use fake;
+        use store::{ReadStore, Row, WriteStore};
+
+        let store = fake::FakeStore {};
+        store.write(vec![Row {
+            key: b"k".to_vec(),
+            value: b"v".to_vec(),
+        }]);
+        store.flush();
+        assert!(store.get(b"").is_none());
+        assert!(store.scan(b"").is_empty());
+    }
+}
