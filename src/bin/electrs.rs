1eb95ce7ea5307653800201e3d5934bea3b00af0
Nadav Ivgi <nadav@shesek.info>
2018-10-29 22:00:54 +0200

    Check initialblockdownload and accept verificationprogress if its close to 1

--- src/bin/electrs.rs
+++ src/bin/electrs.rs
@@ -63,7 +63,8 @@ fn run_server(config: &Config) -> Result<()> {
         query.update_mempool()?;
 
         if server.is_none() {
-            if app.daemon().getblockchaininfo()?.verificationprogress == 1.0 {
+            let info = app.daemon().getblockchaininfo()?;
+            if info.initialblockdownload == true && info.verificationprogress > 0.9999 {
                 server = Some(rest::run_server(&config, query.clone()));
             } else {
                 warn!("bitcoind not fully synced waiting");
