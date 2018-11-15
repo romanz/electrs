5f8eb50b897183a4689a7ccf3487316b49f37490
Roman Zeyde <me@romanzey.de>
2018-11-15 23:10:37 +0200

    Add testnet benchmarking script

---
+++ testnet-bench.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+time curl http://localhost:3001/address/n2zbkoYEUQfXeRmdzzExGxFbyFBGcRAkVL	# 2
+time curl http://localhost:3001/address/mm41aypj76mpTqdRoT5sQ4xrpXYEsGQQ3j	# 15
+time curl http://localhost:3001/address/mkMTj6YCu8pQTLiM5hs2cfS3S1QbSAmyPG	# 59
+time curl http://localhost:3001/address/msYN6FJfvA3p2XoVzgjZpZV4AbEcwBQEEJ	# 396
+time curl http://localhost:3001/address/2NDhzMt2D9ZxXapbuq567WGeWP7NuDN81cg	# 4.5K
+time curl http://localhost:3001/address/msjBEBAqheDdgqVab1mYAh7RULQFjEB9yS	# 8.9K
+time curl http://localhost:3001/address/mxosQ4CvQR8ipfWdRktyB3u16tauEdamGc	# 13K
+time curl http://localhost:3001/address/mgxVT9fzHwYDsgEGJSZekKgYbAyrBkqdpi	# 129K
