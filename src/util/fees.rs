romanz/electrs

commit 8229ceef6c3ab1a0ff5e61c11e6e63ce7b3ddfad

Don't start a new fee bucket until we reach a higher feerate

src/util/fees.rs
@@ -54,13 +54,13 @@ pub fn make_fee_histogram(mut entries: Vec<&TxFeeInfo>) -> Vec<(f32, u32)> {
     let mut bin_size = 0;
     let mut last_fee_rate = None;
     for e in entries.iter().rev() {
-        last_fee_rate = Some(e.fee_per_vbyte);
         bin_size += e.vsize;
-        if bin_size > VSIZE_BIN_WIDTH {
+        if bin_size > VSIZE_BIN_WIDTH && last_fee_rate.map_or(true, |last| e.fee_per_vbyte > last) {
             // vsize of transactions paying >= e.fee_per_vbyte
             histogram.push((e.fee_per_vbyte, bin_size));
             bin_size = 0;
         }
+        last_fee_rate = Some(e.fee_per_vbyte);
     }
     if let Some(fee_rate) = last_fee_rate {
         histogram.push((fee_rate, bin_size));

commit baee77b68ac74d1f2acf3b4e260e22f0ed75958c

Fix fee histogram bug (introduced by 8229ceef6c3ab1a0ff5e61c11e6e63ce7b3ddfad)

src/util/fees.rs
@@ -55,7 +55,7 @@ pub fn make_fee_histogram(mut entries: Vec<&TxFeeInfo>) -> Vec<(f32, u32)> {
     let mut last_fee_rate = None;
     for e in entries.iter().rev() {
         bin_size += e.vsize;
-        if bin_size > VSIZE_BIN_WIDTH && last_fee_rate.map_or(true, |last| e.fee_per_vbyte > last) {
+        if bin_size > VSIZE_BIN_WIDTH && last_fee_rate.map_or(true, |last| e.fee_per_vbyte < last) {
             // vsize of transactions paying >= e.fee_per_vbyte
             histogram.push((e.fee_per_vbyte, bin_size));
             bin_size = 0;