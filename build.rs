5ac315ff3c1ec7e83f05e5dadd449c3dd47e46bd
Martin Habovstiak <martin.habovstiak@gmail.com>
2019-10-18 17:14:53 +0200

    Update `configure_me` as well as other packages
    * This should resolve #180, not sure (couldn't get past rocksdb on 1.34)
    * Uses new, cleaner way of specifying `config_spec.toml`

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 extern crate configure_me_codegen;
 
 fn main() -> Result<(), configure_me_codegen::Error> {
-    configure_me_codegen::build_script_with_man("config_spec.toml")
+    configure_me_codegen::build_script_auto()
 }
