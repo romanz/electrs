romanz/electrs

commit 7112ca3176acfab5bd823e0780100074dd5b16c0

Populate the in-memory HeaderList from the local database
Instead of fetching it from the bitcoind rpc. This speeds up startup times significantly.

This required keeping the last indexed block hash in the db, under `t` (for "tip").
The first start-up without the saved tip will still be slow, but once that gets saved,
followup start-ups will become faster.

The presence of the `t` in the db is now also used to determine whether the initial
sync was completed, replacing the `i` flag introduced in the parent commit.

Resolves https://github.com/Blockstream/esplora/issues/76

src/new_index/schema.rs
@@ -46,19 +46,21 @@ impl Store {
         let headers_map = load_blockheaders(&txstore_db);
         debug!("{} headers were loaded", headers_map.len());
         let cache_db = DB::open(&path.join("cache"));
+
+        let headers = if let Some(tip_hash) = txstore_db.get(b"t") {
+            HeaderList::new(headers_map, deserialize(&tip_hash).expect("invalid tip"))
+        } else {
+            HeaderList::empty()
+        };
+
         Store {
             txstore_db,
             history_db,
             cache_db,
             added_blockhashes: RwLock::new(added_blockhashes),
             indexed_blockhashes: RwLock::new(indexed_blockhashes),
-            indexed_headers: RwLock::new(HeaderList::empty()),
+            indexed_headers: RwLock::new(headers),
         }
-    }
-
-    /// Used to decide whether to use "blk*.dat" files for faster initial indexing
-    pub fn is_empty(&self) -> bool {
-        return self.added_blockhashes.read().unwrap().is_empty();
     }
 
     pub fn txstore_db(&self) -> &DB {
@@ -70,7 +72,7 @@ impl Store {
     }
 
     pub fn done_initial_sync(&self) -> bool {
-        self.txstore_db.get(b"I").is_some()
+        self.txstore_db.get(b"t").is_some()
     }
 }
 
@@ -220,9 +222,11 @@ impl Indexer {
         headers.apply(new_headers);
         assert_eq!(tip, *headers.tip());
 
+        // update the most recently indexed block
+        self.store.txstore_db.put(b"t", &serialize(&tip));
+
         if let FetchFrom::BlkFiles = self.from {
             self.from = FetchFrom::Bitcoind;
-            self.store.txstore_db.put(b"I", &[]);
         }
 
         self.flush = DBFlush::Enable;

src/util/block.rs
@@ -80,6 +80,40 @@ impl HeaderList {
         }
     }
 
+    pub fn new(
+        mut headers_map: HashMap<Sha256dHash, BlockHeader>,
+        tip_hash: Sha256dHash,
+    ) -> HeaderList {
+        trace!(
+            "processing {} headers, tip at {:?}",
+            headers_map.len(),
+            tip_hash
+        );
+
+        let mut blockhash = tip_hash;
+        let mut headers_chain = vec![];
+        let null_hash = Sha256dHash::default();
+
+        while blockhash != null_hash {
+            let header = headers_map
+                .remove(&blockhash)
+                .expect("missing expected blockhash in headers map");
+            blockhash = header.prev_blockhash.clone();
+            headers_chain.push(header);
+        }
+        headers_chain.reverse();
+
+        trace!(
+            "{} chained headers ({} orphan blocks left)",
+            headers_chain.len(),
+            headers_map.len()
+        );
+
+        let mut headers = HeaderList::empty();
+        headers.apply(headers.order(headers_chain));
+        headers
+    }
+
     pub fn order(&self, new_headers: Vec<BlockHeader>) -> Vec<HeaderEntry> {
         // header[i] -> header[i-1] (i.e. header.last() is the tip)
         struct HashedHeader {

commit 4089323aadc63c4a6bc463a04adc9301ddd3f3c7

Fix database inconsistencies with synced block tip
- Make sure new blocks are flushed to disk before updating the synced tip hash, to avoid situations where
  an unclean shutdown causes the tip to be saved while the actual block records are missing (which was likely
  to happen and was reproducible under some cases due to the WAL being disabled for the initial bulk writes,
  while not being disabled for writing the tip hash).

- Flush using `rocksdb::DB::flush()` instead of doing an empty flushed write (which didn't seem to quite work).

This (hopefully!) fixes the issue introduced by the optimization in 7112ca3176acfab5bd823e0780100074dd5b16c0,
which was later disabled in 0a1af17f1c0e72a9a3880b631873722a331e2c7c and re-enabled in 6f28e776e19395403c4cc781682a193aa7c09555.

src/new_index/db.rs
@@ -175,8 +175,18 @@ impl DB {
         self.db.write_opt(batch, &opts).unwrap();
     }
 
+    pub fn flush(&self) {
+        self.db.flush().unwrap();
+    }
+
     pub fn put(&self, key: &[u8], value: &[u8]) {
         self.db.put(key, value).unwrap();
+    }
+
+    pub fn put_sync(&self, key: &[u8], value: &[u8]) {
+        let mut opts = rocksdb::WriteOptions::new();
+        opts.set_sync(true);
+        self.db.put_opt(key, value, &opts).unwrap();
     }
 
     pub fn get(&self, key: &[u8]) -> Option<Bytes> {

src/new_index/schema.rs
@@ -264,20 +264,24 @@ impl Indexer {
         start_fetcher(self.from, &daemon, to_index)?.map(|blocks| self.index(&blocks));
         self.start_auto_compactions(&self.store.history_db);
 
+        if let DBFlush::Disable = self.flush {
+            debug!("flushing to disk");
+            self.store.txstore_db.flush();
+            self.store.history_db.flush();
+            self.flush = DBFlush::Enable;
+        }
+
+        // update the synced tip *after* the new data is flushed to disk
+        debug!("updating synced tip to {:?}", tip);
+        self.store.txstore_db.put_sync(b"t", &serialize(&tip));
+
         let mut headers = self.store.indexed_headers.write().unwrap();
         headers.apply(new_headers);
         assert_eq!(tip, *headers.tip());
 
-        // update the most recently indexed block
-        self.store.txstore_db.put(b"t", &serialize(&tip));
-
         if let FetchFrom::BlkFiles = self.from {
             self.from = FetchFrom::Bitcoind;
         }
-
-        self.flush = DBFlush::Enable;
-        self.store.txstore_db.write(vec![], self.flush);
-        self.store.history_db.write(vec![], self.flush);
 
         Ok(tip)
     }