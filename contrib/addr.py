a02d31eb061e8756ea132f74c0644d1739903f76
Roman Zeyde <me@romanzey.de>
2020-04-11 18:28:45 +0300

    Update contrib/ scripts to use latest PyCoin

--- contrib/addr.py
+++ contrib/addr.py
@@ -3,8 +3,6 @@ import hashlib
 import sys
 import argparse
 
-from pycoin.coins.bitcoin.networks import BitcoinTestnet, BitcoinMainnet
-
 import client
 
 def main():
@@ -14,15 +12,15 @@ def main():
     args = parser.parse_args()
 
     if args.testnet:
-        Network = BitcoinTestnet
         port = 60001
+        from pycoin.symbols.xtn import network
     else:
-        Network = BitcoinMainnet
         port = 50001
+        from pycoin.symbols.btc import network
 
     conn = client.Client(('localhost', port))
     for addr in args.address:
-        script = Network.ui.script_for_address(addr)
+        script = network.parse.address(addr).script()
         script_hash = hashlib.sha256(script).digest()[::-1].hex()
         reply = conn.call('blockchain.scripthash.get_balance', script_hash)
         result = reply['result']
