ef81d98a685cb721e78ab60c6a4e12fdd6d6d774
Nadav Ivgi <nadav@shesek.info>
2020-04-16 14:14:48 +0300

    Fix fee calculation for coinbase transactions
    The buggy behaviour was introduced as part of the refactoring in 14cb089522dc4267c14fd2c070eb7cc3cb5a0625,
    which removed the explicit check for coinbase transactions and caused `total_in - total_out` to overflow.
    
    Resolves https://github.com/Blockstream/esplora/issues/198

--- src/util/fees.rs
+++ src/util/fees.rs
@@ -24,6 +24,10 @@ impl TxFeeInfo {
 
 #[cfg(not(feature = "liquid"))]
 pub fn get_tx_fee(tx: &Transaction, prevouts: &HashMap<u32, &TxOut>, _network: Network) -> u64 {
+    if tx.is_coin_base() {
+        return 0;
+    }
+
     let total_in: u64 = prevouts.values().map(|prevout| prevout.value).sum();
     let total_out: u64 = tx.output.iter().map(|vout| vout.value).sum();
     total_in - total_out
