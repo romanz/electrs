7f085768c9ad1ad99f62a7fbe7e45eeb942e8274
Riccardo Casatta <riccardo@casatta.it>
2020-07-06 15:56:45 +0200

    remove extern crate as in edition 2018 they are not needed

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,5 @@
 #![recursion_limit = "1024"]
 
-extern crate base64;
-extern crate bincode;
-extern crate bitcoin;
-extern crate chan_signal;
-extern crate crypto;
-extern crate dirs;
-extern crate glob;
-extern crate hex;
-extern crate hyper;
-extern crate itertools;
-extern crate libc;
-extern crate num_cpus;
-extern crate page_size;
-extern crate prometheus;
-extern crate rayon;
-extern crate serde;
-extern crate stderrlog;
-extern crate sysconf;
-extern crate time;
-extern crate tiny_http;
-extern crate url;
-
 // See https://github.com/romanz/electrs/issues/193 & https://github.com/rust-rocksdb/rust-rocksdb/issues/327
 #[cfg(not(feature = "oldcpu"))]
 extern crate rocksdb;
