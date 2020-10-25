eb413a0117b950a9baa74d3de05e37cf655e3315
Roman Zeyde <me@romanzey.de>
2020-10-25 14:23:56 +0200

    Add helper scripts

---
+++ query.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eux
+cd `dirname $0`
+
+cargo fmt
+cargo build --all --release
+
+NETWORK=$1
+shift
+
+QUERY="target/release/electrs_query --network $NETWORK --db-dir ./db --daemon-dir $HOME/.bitcoin"
+export RUST_LOG=${RUST_LOG-info}
+$QUERY $*
+
+# use SIGINT to quit
---
+++ server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eux
+cd `dirname $0`
+
+cargo fmt
+cargo build --all --release
+
+NETWORK=$1
+shift
+
+DB=./db  # $HOME/tmp/electrs_db/mainnet_zstd
+QUERY="target/release/electrs_rpc --network $NETWORK --db-dir $DB --daemon-dir $HOME/.bitcoin"
+export RUST_LOG=${RUST_LOG-info}
+$QUERY $*
+
+# use SIGINT to quit
