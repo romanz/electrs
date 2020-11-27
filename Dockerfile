dba7990f48cd3a38767c9e6f766759ee4de08d86
Roman Zeyde <me@romanzey.de>
2020-11-27 21:40:51 +0200

    Add Docker-based integration tests

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,49 @@
-FROM rust:1.44.1-slim-buster
+FROM debian:buster-slim
+RUN apt-get update
 
-RUN apt-get update \
-  && apt-get install -y --no-install-recommends clang=1:7.* cmake=3.* \
-     libsnappy-dev=1.* \
-  && apt-get clean \
-  && rm -rf /var/lib/apt/lists/*
+### Bitcoin ###
 
-RUN adduser --disabled-login --system --shell /bin/false --uid 1000 user
+# Buildtime dependencies
+RUN apt-get install -qqy build-essential libtool autotools-dev automake pkg-config bsdmainutils python3
 
-WORKDIR /home/user
-COPY ./ /home/user
-RUN chown -R user .
+# Runtime dependencies
+RUN apt-get install -qqy libevent-dev libboost-system-dev libboost-filesystem-dev libboost-test-dev libboost-thread-dev
 
-USER user
+# Clone source
+RUN apt-get install -qqy git
+WORKDIR /build/bitcoin
+RUN git clone --branch locations https://github.com/romanz/bitcoin.git .
 
-RUN cargo install --path .
+# Build
+RUN ./autogen.sh && ./configure --disable-wallet --without-gui --without-miniupnpc && make -j"$(($(nproc)+1))"
 
-# Electrum RPC
-EXPOSE 50001
+# Install
+RUN mv -v src/bitcoind src/bitcoin-cli /usr/bin/
 
-# Prometheus monitoring
-EXPOSE 4224
+### Electrum ###
 
-STOPSIGNAL SIGINT
+# Download latest Electrum wallet
+RUN apt-get install -qqy wget libsecp256k1-0 python3-cryptography
+ARG ELECTRUM_VERSION=4.0.5
+WORKDIR /build/electrum
+RUN wget -q https://download.electrum.org/$ELECTRUM_VERSION/Electrum-$ELECTRUM_VERSION.tar.gz \
+&& (echo "6790407e21366186d928c8e653e3ab38476ca86e4797aa4db94dcca2384db41a Electrum-$ELECTRUM_VERSION.tar.gz" | sha256sum -c -)
+
+# Unpack Electrum and install it
+RUN tar xfz Electrum-$ELECTRUM_VERSION.tar.gz && ln -s $PWD/Electrum-$ELECTRUM_VERSION/run_electrum /usr/bin/electrum
+RUN electrum version --offline
+
+### Electrum Rust Server ###
+
+# Install Rust 1.41.1
+RUN apt-get install -qq -y clang cmake cargo rustc
+
+# Build electrs and install it
+WORKDIR /build/electrs
+COPY . .
+RUN cargo build --locked --release -p electrs_rpc
+RUN cp target/release/electrs_rpc /usr/bin/electrs
+
+# Install a few test tools and copy the integration tests
+RUN apt-get install -qqy jq netcat
+
---
+++ tests/run.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+set -euo pipefail
+
+mkdir -p data/{bitcoin,electrum,electrs}
+
+BTC="bitcoin-cli -regtest -datadir=data/bitcoin"
+ELECTRUM="electrum --regtest"
+EL="$ELECTRUM --wallet data/electrum/wallet"
+
+echo "Starting $(bitcoind -version | head -n1)..."
+bitcoind -regtest -datadir=data/bitcoin -printtoconsole=0 &
+
+$BTC -rpcwait getblockcount > /dev/null
+
+echo "Creating Electrum `electrum version --offline` wallet..."
+WALLET=`$EL create --offline --seed_type segwit`
+MINING_ADDR=`$EL --offline getunusedaddress`
+
+$BTC generatetoaddress 110 $MINING_ADDR > /dev/null
+echo `$BTC getblockchaininfo | jq -r '"Generated \(.blocks) regtest blocks (\(.size_on_disk/1e3) kB)"'` to $MINING_ADDR
+
+TIP=`$BTC getbestblockhash`
+$BTC getblocklocations $TIP 1000 > /dev/null  # make sure the new RPC works
+
+echo "Starting `electrs -V`..."
+export RUST_LOG=electrs=debug
+electrs --db-dir=data/electrs --daemon-dir=data/bitcoin --network=regtest 2> data/electrs/regtest-debug.log &
+sleep 1
+
+TIP_HEADER=`contrib/get_tip.py localhost 60401`
+test `jq -r .height <<< "$TIP_HEADER"` == 110  # Make sure electrs is synced to the tip
+test `jq -r .hex <<< "$TIP_HEADER"` == `$BTC getblockheader $TIP false`
+
+echo "Starting Electrum daemon..."
+$ELECTRUM daemon --server localhost:60401:t -1 -vDEBUG 2> data/electrum/regtest-debug.log &
+sleep 1
+
+echo "Loading Electrum wallet..."
+test `$EL load_wallet` == "true"
+sleep 1
+
+$EL getinfo | jq -c .
+
+echo "Running integration tests:"
+
+echo " * getbalance"
+test "`$EL getbalance | jq -c .`" == '{"confirmed":"550","unmatured":"4950"}'
+
+echo " * getunusedaddress"
+NEW_ADDR=`$EL getunusedaddress`
+
+echo " * payto & broadcast"
+TXID=$($EL broadcast $($EL payto $NEW_ADDR 123 --fee 0.001))
+
+echo " * get_tx_status"
+test "`$EL get_tx_status $TXID | jq -c .`" == '{"confirmations":0}'
+
+echo " * getaddresshistory"
+test "`$EL getaddresshistory $NEW_ADDR | jq -c .`" == "[{\"fee\":100000,\"height\":0,\"tx_hash\":\"$TXID\"}]"
+
+echo " * getbalance"
+test "`$EL getbalance | jq -c .`" == '{"confirmed":"550","unconfirmed":"-0.001","unmatured":"4950"}'
+
+echo "Generating bitcoin block..."
+$BTC generatetoaddress 1 $MINING_ADDR > /dev/null
+$BTC getblockcount > /dev/null
+
+echo " * get_tx_status"
+test "`$EL get_tx_status $TXID | jq -c .`" == '{"confirmations":1}'
+
+echo " * getaddresshistory"
+test "`$EL getaddresshistory $NEW_ADDR | jq -c .`" == "[{\"fee\":null,\"height\":111,\"tx_hash\":\"$TXID\"}]"
+
+echo " * getbalance"
+test "`$EL getbalance | jq -c .`" == '{"confirmed":"599.999","unmatured":"4950.001"}'
