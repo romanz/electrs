cca0f270c2f47d6e3664a501745ee15df350f68d
Roman Zeyde <me@romanzey.de>
2021-05-30 09:29:09 +0300

    Run sync in a loop

--- src/bin/sync.rs
+++ src/bin/sync.rs
@@ -5,5 +5,9 @@ use electrs::{Config, Daemon, Tracker};
 fn main() -> Result<()> {
     let config = Config::from_args();
     let daemon = Daemon::connect(&config)?;
-    Tracker::new(&config)?.sync(&daemon)
+    let mut tracker = Tracker::new(&config)?;
+    loop {
+        tracker.sync(&daemon)?;
+        std::thread::sleep(config.wait_duration);
+    }
 }
