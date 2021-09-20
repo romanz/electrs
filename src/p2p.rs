3041e89cd2fb377541b929d852ef6298c2d4e60a
Roman Zeyde <me@romanzey.de>
2021-09-20 17:39:48 +0300

    Bump bitcoin crate to 0.27.1
    Fixes #490.
    Also, reverts 52dd59c117f7a8c06fdb7f023146bcd9f6d40b5c.

--- src/p2p.rs
+++ src/p2p.rs
@@ -47,7 +47,7 @@ impl Connection {
     }
 
     fn send(&mut self, msg: NetworkMessage) -> Result<()> {
-        trace!("send: {}", msg.cmd());
+        trace!("send: {:?}", msg);
         let raw_msg = message::RawNetworkMessage {
             magic: self.network.magic(),
             payload: msg,
@@ -62,7 +62,7 @@ impl Connection {
             let raw_msg: message::RawNetworkMessage =
                 self.reader.read_next().context("p2p failed to recv")?;
 
-            trace!("recv: {}", raw_msg.cmd());
+            trace!("recv: {:?}", raw_msg.payload);
             match raw_msg.payload {
                 NetworkMessage::Version(version) => {
                     debug!("peer version: {:?}", version);
