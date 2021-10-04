9a45d9c7984277d644e299677741111df25ad6e4
Roman Zeyde <me@romanzey.de>
2021-10-04 14:19:28 +0300

    Fix cache methods' scope

--- src/cache.rs
+++ src/cache.rs
@@ -7,24 +7,24 @@ use std::sync::Arc;
 use crate::merkle::Proof;
 
 #[derive(Default)]
-pub struct Cache {
+pub(crate) struct Cache {
     txs: Arc<RwLock<HashMap<Txid, Transaction>>>,
     proofs: Arc<RwLock<HashMap<(BlockHash, Txid), Proof>>>,
 }
 
 impl Cache {
-    pub(crate) fn add_tx(&self, txid: Txid, f: impl FnOnce() -> Transaction) {
+    pub fn add_tx(&self, txid: Txid, f: impl FnOnce() -> Transaction) {
         self.txs.write().entry(txid).or_insert_with(f);
     }
 
-    pub(crate) fn get_tx<F, T>(&self, txid: &Txid, f: F) -> Option<T>
+    pub fn get_tx<F, T>(&self, txid: &Txid, f: F) -> Option<T>
     where
         F: FnOnce(&Transaction) -> T,
     {
         self.txs.read().get(txid).map(f)
     }
 
-    pub(crate) fn add_proof<F>(&self, blockhash: BlockHash, txid: Txid, f: F)
+    pub fn add_proof<F>(&self, blockhash: BlockHash, txid: Txid, f: F)
     where
         F: FnOnce() -> Proof,
     {
@@ -34,7 +34,7 @@ impl Cache {
             .or_insert_with(f);
     }
 
-    pub(crate) fn get_proof<F, T>(&self, blockhash: BlockHash, txid: Txid, f: F) -> Option<T>
+    pub fn get_proof<F, T>(&self, blockhash: BlockHash, txid: Txid, f: F) -> Option<T>
     where
         F: FnOnce(&Proof) -> T,
     {
