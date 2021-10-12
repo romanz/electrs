cd0531b8b771ce77054e38053f48e8756aff07ff
Roman Zeyde <me@romanzey.de>
2021-10-12 18:49:24 +0300

    Copy StreamReader implementation into our crate

--- src/p2p.rs
+++ src/p2p.rs
@@ -1,19 +1,18 @@
 use anyhow::{Context, Result};
 use bitcoin::{
-    consensus::encode,
+    consensus::{encode, Decodable},
     network::{
         address, constants,
         message::{self, NetworkMessage},
         message_blockdata::{GetHeadersMessage, Inventory},
         message_network,
-        stream_reader::StreamReader,
     },
     secp256k1::{self, rand::Rng},
     Block, BlockHash, BlockHeader, Network,
 };
 use crossbeam_channel::{bounded, select, Receiver, Sender};
 
-use std::io::Write;
+use std::io::{ErrorKind, Read, Write};
 use std::iter::FromIterator;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream};
 use std::time::{SystemTime, UNIX_EPOCH};
@@ -181,7 +180,6 @@ impl Connection {
 
         crate::thread::spawn("p2p_recv", move || loop {
             use bitcoin::consensus::encode::Error;
-            use std::io::ErrorKind;
 
             let raw_msg: message::RawNetworkMessage =
                 match recv_duration.observe_duration("recv", || reader.read_next()) {
@@ -300,5 +298,53 @@ fn is_block_inv(inv: &Inventory) -> bool {
         true
     } else {
         false
+    }
+}
+
+// Struct used to configure stream reader function
+struct StreamReader<R: Read> {
+    /// Stream to read from
+    pub stream: R,
+    /// I/O buffer
+    data: Vec<u8>,
+    /// Buffer containing unparsed message part
+    unparsed: Vec<u8>,
+}
+
+impl<R: Read> StreamReader<R> {
+    /// Constructs new stream reader for a given input stream `stream` with
+    /// optional parameter `buffer_size` determining reading buffer size
+    pub fn new(stream: R, buffer_size: Option<usize>) -> StreamReader<R> {
+        StreamReader {
+            stream,
+            data: vec![0u8; buffer_size.unwrap_or(64 * 1024)],
+            unparsed: vec![],
+        }
+    }
+
+    /// Reads stream and parses next message from its current input,
+    /// also taking into account previously unparsed partial message (if there was such).
+    pub fn read_next<D: Decodable>(&mut self) -> Result<D, encode::Error> {
+        loop {
+            match encode::deserialize_partial::<D>(&self.unparsed) {
+                // In this case we just have an incomplete data, so we need to read more
+                Err(encode::Error::Io(ref err)) if err.kind() == ErrorKind::UnexpectedEof => {
+                    let count = self.stream.read(&mut self.data)?;
+                    if count > 0 {
+                        self.unparsed.extend(self.data[0..count].iter());
+                    } else {
+                        return Err(encode::Error::Io(std::io::Error::from(
+                            ErrorKind::UnexpectedEof,
+                        )));
+                    }
+                }
+                Err(err) => return Err(err),
+                // We have successfully read from the buffer
+                Ok((message, index)) => {
+                    self.unparsed.drain(..index);
+                    return Ok(message);
+                }
+            }
+        }
     }
 }
