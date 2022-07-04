e40c106bfc88c377d703ccd4dd0e9159f16271d5
Khaled Emara <mail@KhaledEmara.dev>
2022-07-04 16:07:19 +0200

    Replace RocksDB with sled

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,10 @@
-use anyhow::{Context, Result};
-use electrs_rocksdb as rocksdb;
+extern crate sled;
 
+use anyhow::{Context, Result};
+use sled::Transactional;
+
+use std::fs::remove_dir_all;
+use std::iter;
 use std::path::Path;
 use std::sync::atomic::{AtomicBool, Ordering};
 
@@ -24,59 +28,28 @@ impl WriteBatch {
     }
 }
 
-/// RocksDB wrapper for index storage
+/// sled wrapper for index storage
 pub struct DBStore {
-    db: rocksdb::DB,
+    db: sled::Db,
     bulk_import: AtomicBool,
 }
 
-const CONFIG_CF: &str = "config";
-const HEADERS_CF: &str = "headers";
-const TXID_CF: &str = "txid";
-const FUNDING_CF: &str = "funding";
-const SPENDING_CF: &str = "spending";
+const CONFIG_TREE: &str = "config";
+const HEADERS_TREE: &str = "headers";
+const TXID_TREE: &str = "txid";
+const FUNDING_TREE: &str = "funding";
+const SPENDING_TREE: &str = "spending";
 
-const COLUMN_FAMILIES: &[&str] = &[CONFIG_CF, HEADERS_CF, TXID_CF, FUNDING_CF, SPENDING_CF];
+const TREES: &[&str] = &[
+    CONFIG_TREE,
+    HEADERS_TREE,
+    TXID_TREE,
+    FUNDING_TREE,
+    SPENDING_TREE,
+];
 
 const CONFIG_KEY: &str = "C";
 const TIP_KEY: &[u8] = b"T";
-
-// Taken from https://github.com/facebook/rocksdb/blob/master/include/rocksdb/db.h#L654-L689
-const DB_PROPERIES: &[&str] = &[
-    "rocksdb.num-immutable-mem-table",
-    "rocksdb.mem-table-flush-pending",
-    "rocksdb.compaction-pending",
-    "rocksdb.background-errors",
-    "rocksdb.cur-size-active-mem-table",
-    "rocksdb.cur-size-all-mem-tables",
-    "rocksdb.size-all-mem-tables",
-    "rocksdb.num-entries-active-mem-table",
-    "rocksdb.num-entries-imm-mem-tables",
-    "rocksdb.num-deletes-active-mem-table",
-    "rocksdb.num-deletes-imm-mem-tables",
-    "rocksdb.estimate-num-keys",
-    "rocksdb.estimate-table-readers-mem",
-    "rocksdb.is-file-deletions-enabled",
-    "rocksdb.num-snapshots",
-    "rocksdb.oldest-snapshot-time",
-    "rocksdb.num-live-versions",
-    "rocksdb.current-super-version-number",
-    "rocksdb.estimate-live-data-size",
-    "rocksdb.min-log-number-to-keep",
-    "rocksdb.min-obsolete-sst-number-to-keep",
-    "rocksdb.total-sst-files-size",
-    "rocksdb.live-sst-files-size",
-    "rocksdb.base-level",
-    "rocksdb.estimate-pending-compaction-bytes",
-    "rocksdb.num-running-compactions",
-    "rocksdb.num-running-flushes",
-    "rocksdb.actual-delayed-write-rate",
-    "rocksdb.is-write-stopped",
-    "rocksdb.estimate-oldest-key-time",
-    "rocksdb.block-cache-capacity",
-    "rocksdb.block-cache-usage",
-    "rocksdb.block-cache-pinned-usage",
-];
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Config {
@@ -95,42 +68,29 @@ impl Default for Config {
     }
 }
 
-fn default_opts() -> rocksdb::Options {
-    let mut opts = rocksdb::Options::default();
-    opts.set_keep_log_file_num(10);
-    opts.set_max_open_files(16);
-    opts.set_compaction_style(rocksdb::DBCompactionStyle::Level);
-    opts.set_compression_type(rocksdb::DBCompressionType::Zstd);
-    opts.set_target_file_size_base(256 << 20);
-    opts.set_write_buffer_size(256 << 20);
-    opts.set_disable_auto_compactions(true); // for initial bulk load
-    opts.set_advise_random_on_open(false); // bulk load uses sequential I/O
-    opts.set_prefix_extractor(rocksdb::SliceTransform::create_fixed_prefix(8));
-    opts
+fn default_db_config(path: &Path) -> sled::Config {
+    sled::Config::default()
+        .path(path)
+        .cache_capacity(10_000_000_000)
+        .mode(sled::Mode::HighThroughput)
+        .use_compression(false)
+        .compression_factor(11)
+        .temporary(false)
+        .flush_every_ms(Some(1000))
 }
 
 impl DBStore {
-    fn create_cf_descriptors() -> Vec<rocksdb::ColumnFamilyDescriptor> {
-        COLUMN_FAMILIES
-            .iter()
-            .map(|&name| rocksdb::ColumnFamilyDescriptor::new(name, default_opts()))
-            .collect()
-    }
-
     fn open_internal(path: &Path) -> Result<Self> {
-        let mut db_opts = default_opts();
-        db_opts.create_if_missing(true);
-        db_opts.create_missing_column_families(true);
+        let db_config = default_db_config(path);
 
-        let db = rocksdb::DB::open_cf_descriptors(&db_opts, path, Self::create_cf_descriptors())
+        let db = db_config
+            .open()
             .with_context(|| format!("failed to open DB: {}", path.display()))?;
-        let live_files = db.live_files()?;
         info!(
-            "{:?}: {} SST files, {} GB, {} Grows",
+            "{:?}: {} Trees, {} GB",
             path,
-            live_files.len(),
-            live_files.iter().map(|f| f.size).sum::<usize>() as f64 / 1e9,
-            live_files.iter().map(|f| f.num_entries).sum::<u64>() as f64 / 1e9
+            db.tree_names().len(),
+            db.size_on_disk().unwrap_or_default()
         );
         let store = DBStore {
             db,
@@ -139,24 +99,14 @@ impl DBStore {
         Ok(store)
     }
 
-    fn is_legacy_format(&self) -> bool {
-        // In legacy DB format, all data was stored in a single (default) column family.
-        self.db
-            .iterator(rocksdb::IteratorMode::Start)
-            .next()
-            .is_some()
-    }
-
-    /// Opens a new RocksDB at the specified location.
+    /// Opens a new sled DB at the specified location.
     pub fn open(path: &Path, auto_reindex: bool) -> Result<Self> {
         let mut store = Self::open_internal(path)?;
         let config = store.get_config();
         debug!("DB {:?}", config);
         let mut config = config.unwrap_or_default(); // use default config when DB is empty
 
-        let reindex_cause = if store.is_legacy_format() {
-            Some("legacy format".to_owned())
-        } else if config.format != CURRENT_FORMAT {
+        let reindex_cause = if config.format != CURRENT_FORMAT {
             Some(format!(
                 "unsupported format {} != {}",
                 config.format, CURRENT_FORMAT
@@ -173,176 +123,150 @@ impl DBStore {
                 cause,
                 path.display()
             );
-            // close DB before deletion
+            let tree_names = store.db.tree_names();
+            for name in tree_names.iter() {
+                store.db.drop_tree(name).with_context(|| {
+                    format!(
+                        "re-index required but the old database ({}) can not be deleted",
+                        path.display()
+                    )
+                })?;
+            }
             drop(store);
-            rocksdb::DB::destroy(&default_opts(), &path).with_context(|| {
-                format!(
-                    "re-index required but the old database ({}) can not be deleted",
-                    path.display()
-                )
-            })?;
+            remove_dir_all(path).unwrap();
             store = Self::open_internal(path)?;
-            config = Config::default(); // re-init config after dropping DB
-        }
-        if config.compacted {
-            store.start_compactions();
+            config = Config::default();
         }
         store.set_config(config);
         Ok(store)
     }
 
-    fn config_cf(&self) -> &rocksdb::ColumnFamily {
-        self.db.cf_handle(CONFIG_CF).expect("missing CONFIG_CF")
+    fn config_tree(&self) -> sled::Tree {
+        self.db.open_tree(CONFIG_TREE).expect("missing CONFIG_TREE")
     }
 
-    fn funding_cf(&self) -> &rocksdb::ColumnFamily {
-        self.db.cf_handle(FUNDING_CF).expect("missing FUNDING_CF")
+    fn funding_tree(&self) -> sled::Tree {
+        self.db
+            .open_tree(FUNDING_TREE)
+            .expect("missing FUNDING_TREE")
     }
 
-    fn spending_cf(&self) -> &rocksdb::ColumnFamily {
-        self.db.cf_handle(SPENDING_CF).expect("missing SPENDING_CF")
+    fn spending_tree(&self) -> sled::Tree {
+        self.db
+            .open_tree(SPENDING_TREE)
+            .expect("missing SPENDING_TREE")
     }
 
-    fn txid_cf(&self) -> &rocksdb::ColumnFamily {
-        self.db.cf_handle(TXID_CF).expect("missing TXID_CF")
+    fn txid_tree(&self) -> sled::Tree {
+        self.db.open_tree(TXID_TREE).expect("missing TXID_TREE")
     }
 
-    fn headers_cf(&self) -> &rocksdb::ColumnFamily {
-        self.db.cf_handle(HEADERS_CF).expect("missing HEADERS_CF")
+    fn headers_tree(&self) -> sled::Tree {
+        self.db
+            .open_tree(HEADERS_TREE)
+            .expect("missing HEADERS_TREE")
     }
 
     pub(crate) fn iter_funding(&self, prefix: Row) -> impl Iterator<Item = Row> + '_ {
-        self.iter_prefix_cf(self.funding_cf(), prefix)
+        self.iter_prefix_tree(&self.funding_tree(), prefix)
     }
 
     pub(crate) fn iter_spending(&self, prefix: Row) -> impl Iterator<Item = Row> + '_ {
-        self.iter_prefix_cf(self.spending_cf(), prefix)
+        self.iter_prefix_tree(&self.spending_tree(), prefix)
     }
 
     pub(crate) fn iter_txid(&self, prefix: Row) -> impl Iterator<Item = Row> + '_ {
-        self.iter_prefix_cf(self.txid_cf(), prefix)
+        self.iter_prefix_tree(&self.txid_tree(), prefix)
     }
 
-    fn iter_prefix_cf(
-        &self,
-        cf: &rocksdb::ColumnFamily,
-        prefix: Row,
-    ) -> impl Iterator<Item = Row> + '_ {
-        let mode = rocksdb::IteratorMode::From(&prefix, rocksdb::Direction::Forward);
-        let mut opts = rocksdb::ReadOptions::default();
-        opts.set_prefix_same_as_start(true); // requires .set_prefix_extractor() above.
-        self.db
-            .iterator_cf_opt(cf, opts, mode)
-            .map(|(key, _value)| key) // values are empty in prefix-scanned CFs
+    fn iter_prefix_tree(&self, tree: &sled::Tree, prefix: Row) -> impl Iterator<Item = Row> + '_ {
+        let iter = tree.scan_prefix::<Row>(prefix);
+        iter.keys().map(|key| Row::from(&key.unwrap()[..]))
     }
 
     pub(crate) fn read_headers(&self) -> Vec<Row> {
-        let mut opts = rocksdb::ReadOptions::default();
-        opts.fill_cache(false);
-        self.db
-            .iterator_cf_opt(self.headers_cf(), opts, rocksdb::IteratorMode::Start)
-            .map(|(key, _)| key)
+        let iter = self.headers_tree().iter();
+        iter.keys()
+            .map(|key| Row::from(&key.unwrap()[..]))
             .filter(|key| &key[..] != TIP_KEY) // headers' rows are longer than TIP_KEY
             .collect()
     }
 
     pub(crate) fn get_tip(&self) -> Option<Vec<u8>> {
-        self.db
-            .get_cf(self.headers_cf(), TIP_KEY)
-            .expect("get_tip failed")
+        let res = self.headers_tree().get(TIP_KEY).expect("get_tip failed");
+
+        if let Some(tip) = res {
+            Some(tip.to_vec())
+        } else {
+            None
+        }
     }
 
     pub(crate) fn write(&self, batch: &WriteBatch) {
-        let mut db_batch = rocksdb::WriteBatch::default();
-        for key in &batch.funding_rows {
-            db_batch.put_cf(self.funding_cf(), key, b"");
-        }
-        for key in &batch.spending_rows {
-            db_batch.put_cf(self.spending_cf(), key, b"");
-        }
-        for key in &batch.txid_rows {
-            db_batch.put_cf(self.txid_cf(), key, b"");
-        }
-        for key in &batch.header_rows {
-            db_batch.put_cf(self.headers_cf(), key, b"");
-        }
-        db_batch.put_cf(self.headers_cf(), TIP_KEY, &batch.tip_row);
+        let _res: sled::transaction::TransactionError<()> = (
+            &self.funding_tree(),
+            &self.spending_tree(),
+            &self.txid_tree(),
+            &self.headers_tree(),
+        )
+            .transaction(|(funding_tree, spending_tree, txid_tree, headers_tree)| {
+                let mut db_batch = sled::Batch::default();
+                for key in &batch.funding_rows {
+                    db_batch.insert(&key[..], b"");
+                }
+                funding_tree.apply_batch(&db_batch)?;
 
-        let mut opts = rocksdb::WriteOptions::new();
-        let bulk_import = self.bulk_import.load(Ordering::Relaxed);
-        opts.set_sync(!bulk_import);
-        opts.disable_wal(bulk_import);
-        self.db.write_opt(db_batch, &opts).unwrap();
+                let mut db_batch = sled::Batch::default();
+                for key in &batch.spending_rows {
+                    db_batch.insert(&key[..], b"");
+                }
+                spending_tree.apply_batch(&db_batch)?;
+
+                let mut db_batch = sled::Batch::default();
+                for key in &batch.txid_rows {
+                    db_batch.insert(&key[..], b"");
+                }
+                txid_tree.apply_batch(&db_batch)?;
+
+                let mut db_batch = sled::Batch::default();
+                for key in &batch.header_rows {
+                    db_batch.insert(&key[..], b"");
+                }
+                let key = TIP_KEY;
+                let value = &(*batch.tip_row);
+                db_batch.insert(key, value);
+                headers_tree.apply_batch(&db_batch)?;
+
+                Ok(())
+            })
+            .unwrap_err();
     }
 
     pub(crate) fn flush(&self) {
-        let mut config = self.get_config().unwrap_or_default();
-        for name in COLUMN_FAMILIES {
-            let cf = self.db.cf_handle(name).expect("missing CF");
-            self.db.flush_cf(cf).expect("CF flush failed");
-        }
-        if !config.compacted {
-            for name in COLUMN_FAMILIES {
-                info!("starting {} compaction", name);
-                let cf = self.db.cf_handle(name).expect("missing CF");
-                self.db.compact_range_cf(cf, None::<&[u8]>, None::<&[u8]>);
-            }
-            config.compacted = true;
-            self.set_config(config);
-            info!("finished full compaction");
-            self.start_compactions();
-        }
-        if log_enabled!(log::Level::Trace) {
-            for property in &["rocksdb.dbstats"] {
-                let stats = self
-                    .db
-                    .property_value(property)
-                    .expect("failed to get property")
-                    .expect("missing property");
-                trace!("{}: {}", property, stats);
-            }
+        for name in TREES {
+            let tree = self.db.open_tree(name).expect("missing TREE");
+            let _bulk_import = self.bulk_import.load(Ordering::Relaxed);
+
+            tree.flush().expect("TREE flush failed");
         }
     }
 
     pub(crate) fn get_properties(
         &self,
     ) -> impl Iterator<Item = (&'static str, &'static str, u64)> + '_ {
-        COLUMN_FAMILIES.iter().flat_map(move |cf_name| {
-            let cf = self.db.cf_handle(cf_name).expect("missing CF");
-            DB_PROPERIES.iter().filter_map(move |property_name| {
-                let value = self
-                    .db
-                    .property_int_value_cf(cf, property_name)
-                    .expect("failed to get property");
-                Some((*cf_name, *property_name, value?))
-            })
-        })
-    }
-
-    fn start_compactions(&self) {
-        self.bulk_import.store(false, Ordering::Relaxed);
-        for name in COLUMN_FAMILIES {
-            let cf = self.db.cf_handle(name).expect("missing CF");
-            self.db
-                .set_options_cf(cf, &[("disable_auto_compactions", "false")])
-                .expect("failed to start auto-compactions");
-        }
-        debug!("auto-compactions enabled");
+        iter::empty::<(&'static str, &'static str, u64)>()
     }
 
     fn set_config(&self, config: Config) {
-        let mut opts = rocksdb::WriteOptions::default();
-        opts.set_sync(true);
-        opts.disable_wal(false);
         let value = serde_json::to_vec(&config).expect("failed to serialize config");
-        self.db
-            .put_cf_opt(self.config_cf(), CONFIG_KEY, value, &opts)
+        self.config_tree()
+            .insert(CONFIG_KEY, value)
             .expect("DB::put failed");
     }
 
     fn get_config(&self) -> Option<Config> {
-        self.db
-            .get_cf(self.config_cf(), CONFIG_KEY)
+        self.config_tree()
+            .get(CONFIG_KEY)
             .expect("DB::get failed")
             .map(|value| serde_json::from_slice(&value).expect("failed to deserialize Config"))
     }
@@ -350,13 +274,13 @@ impl DBStore {
 
 impl Drop for DBStore {
     fn drop(&mut self) {
-        info!("closing DB at {}", self.db.path().display());
+        info!("closing DB");
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{rocksdb, DBStore, WriteBatch, CURRENT_FORMAT};
+    use super::{DBStore, WriteBatch, CURRENT_FORMAT};
 
     #[test]
     fn test_reindex_new_format() {
@@ -374,28 +298,6 @@ mod tests {
                 CURRENT_FORMAT + 1,
                 CURRENT_FORMAT
             )
-        );
-        {
-            let store = DBStore::open(dir.path(), true).unwrap();
-            store.flush();
-            let config = store.get_config().unwrap();
-            assert_eq!(config.format, CURRENT_FORMAT);
-            assert_eq!(store.is_legacy_format(), false);
-        }
-    }
-
-    #[test]
-    fn test_reindex_legacy_format() {
-        let dir = tempfile::tempdir().unwrap();
-        {
-            let mut db_opts = rocksdb::Options::default();
-            db_opts.create_if_missing(true);
-            let db = rocksdb::DB::open(&db_opts, dir.path()).unwrap();
-            db.put(b"F", b"").unwrap(); // insert legacy DB compaction marker (in 'default' column family)
-        };
-        assert_eq!(
-            DBStore::open(dir.path(), false).err().unwrap().to_string(),
-            format!("re-index required due to legacy format",)
         );
         {
             let store = DBStore::open(dir.path(), true).unwrap();
