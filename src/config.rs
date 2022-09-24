d9c4938886ed43a7076d333ca3e218d4035fed50
Roman Zeyde <me@romanzey.de>
2022-09-24 19:58:39 +0300

    Fixup signet magic error message

--- src/config.rs
+++ src/config.rs
@@ -234,7 +234,13 @@ impl Config {
 
         let magic = match (config.network, config.signet_magic) {
             (Network::Signet, Some(magic)) => u32::from_str_radix(&magic, 16)
-                .expect("Invalid signet hex magic")
+                .unwrap_or_else(|error| {
+                    eprintln!(
+                        "Error: signet magic '{}' is not a valid hex string: {}",
+                        magic, error
+                    );
+                    std::process::exit(1);
+                })
                 .swap_bytes(),
             (network, None) => network.magic(),
             (_, Some(_)) => {
