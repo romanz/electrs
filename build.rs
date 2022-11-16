23618513b7e841430f6ff7dad733847852fdb40a
Martin Habovstiak <martin.habovstiak@gmail.com>
2022-11-17 00:59:17 +0100

    Update `configure_me_codegen`
    Improves developer experience by outputting nice error messages when
    compiled with `--features=configure_me_codegen/spanned`.

--- build.rs
+++ build.rs
@@ -1,5 +1,3 @@
-extern crate configure_me_codegen;
-
-fn main() -> Result<(), configure_me_codegen::Error> {
-    configure_me_codegen::build_script_auto()
+fn main() {
+    configure_me_codegen::build_script_auto().unwrap_or_else(|error| error.report_and_exit())
 }
