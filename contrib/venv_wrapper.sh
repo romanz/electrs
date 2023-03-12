3d7c35b0a413308a6b830c962e4a3235297a4090
jroulin <92548716+jroulin@users.noreply.github.com>
2023-03-12 21:44:00 +0100

    contrib: rename xpub into get_balance (#627)
    * rename xpub into get_balance
    
    - support a single address or an xpub address
    - detail the balance per address when using an xpub
    
    * Replace 'virtualenv' by 'python -m venv'
    
    ---------
    
    Co-authored-by: Roman Zeyde <me@romanzey.de>

--- contrib/venv_wrapper.sh
+++ contrib/venv_wrapper.sh
@@ -9,7 +9,7 @@ cmd="$(basename -- "${BASH_SOURCE[0]}" .sh)".py
 if [ "$1" = "--venv" ]; then
     shift
     if [ ! -d .venv ]; then
-        virtualenv .venv
+        python -m venv .venv
         .venv/bin/pip install pycoin logbook prettytable base58
     fi
     PATH=$PWD/.venv/bin:$PATH
